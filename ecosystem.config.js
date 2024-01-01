76b9b43082f1e62d79eec2c7544a7fe52e6cb2fc
Kuma <kuma@millie.town>
2024-01-01 17:01:58 +0900

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
             'ref': 'origin/master',
             'repo': 'git@github.com:AnHeung/node-express-server.git',
             'path': '/home/kuma/workspace/sample',
-            'post-deploy': 'npm install && pm2 start'
+            'post-deploy': 'yarn && pm2 start'
         }
     }
 }
