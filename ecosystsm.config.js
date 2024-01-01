dcc02f667893582804ee73fcda7940255cd4c746
Kuma <kuma@millie.town>
2024-01-01 17:00:25 +0900

    test

--- ecosystsm.config.js
+++ ecosystsm.config.js
@@ -1,16 +1,16 @@
 module.exports = {
     apps: [{
-        name: "sample",
-        script: "./src/index.js",
+        name: 'sample',
+        script: './src/index.js',
     }],
     deploy: {
         production: {
-            "user": "kuma",
-            "host": ["192.168.0.155"],
-            "ref": "origin/master",
-            "repo": "git@github.com:AnHeung/node-express-server.git",
-            "path": "/home/kuma/workspace/sample",
-            "post-deploy": "bash npm install && bash pm2 start"
+            'user': 'kuma',
+            'host': ['192.168.0.155'],
+            'ref': 'origin/master',
+            'repo': 'git@github.com:AnHeung/node-express-server.git',
+            'path': '/home/kuma/workspace/sample',
+            'post-deploy': 'bash npm install && bash pm2 start'
         }
     }
 }
