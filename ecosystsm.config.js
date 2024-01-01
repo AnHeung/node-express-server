51b118fd1f070899afdc0046249e4f53d5de7d0a
Kuma <kuma@millie.town>
2024-01-01 16:10:53 +0900

    deploy add

--- ecosystsm.config.js
+++ ecosystsm.config.js
@@ -8,5 +8,15 @@ module.exports = {
         env_development: {
             NODE_ENV: "development"
         }
-    }]
+    }],
+    deploy: {
+        production: {
+            "user": "ubuntu",
+            "host": ["192.168.0.155"],
+            "ref": "origin/master",
+            "repo": "git@github.com:AnHeung/node-express-server.git",
+            "path": "/home/kuma/workspace/",
+            "post-deploy": "yarn"
+        }
+    }
 }
