a51d96d74a94be90b8aa9b3d1a7e83b92f36b1aa
Kuma <kuma@millie.town>
2024-01-01 16:55:04 +0900

    tst

--- ecosystsm.config.js
+++ ecosystsm.config.js
@@ -2,12 +2,6 @@ module.exports = {
     apps: [{
         name: "sample",
         script: "./src/index.js",
-        env_production: {
-            NODE_ENV: "production"
-        },
-        env_development: {
-            NODE_ENV: "development"
-        }
     }],
     deploy: {
         production: {
